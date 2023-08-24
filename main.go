fbad17adbf139ed287049a53c7b2644fdfec4cc1
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-08-24 23:00:54 +0300

    add echo handler

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
   app := fiber.New()
 
   app.Static("/", "./public")
+
+  app.Post("/echo", func(c *fiber.Ctx) error {
+    return c.Send(c.Body())
+  })
 
   app.Listen(":3000")
 }
